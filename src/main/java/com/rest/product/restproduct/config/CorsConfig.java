348410a4a2a46c83a50cfddde06387641a6c5b21
SarthakN <sarthaknayyar6@gmail.com>
2024-05-22 14:32:43 +0530

    final

--- src/main/java/com/rest/product/restproduct/config/CorsConfig.java
+++ src/main/java/com/rest/product/restproduct/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173,localhost:5174,localhost:5175") 
+            .allowedOrigins("http://localhost:5173,localhost:5174,localhost:5175,masterpiecemart.vercel.app,https://masterpiecemart.vercel.app,https://masterpiecemart.vercel.app/") 
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
